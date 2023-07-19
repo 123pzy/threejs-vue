d8e6736e671e8e6a3199f9dac7d552fd94ebb2d1
PanpanIsCoding <zeyupan@163.com>
2023-07-19 17:19:00 +0800

    测试

--- src/components/SomeGeometry.vue
+++ src/components/SomeGeometry.vue
@@ -66,7 +66,6 @@ gui.add({ radius: 1 }, "radius", 0, 10).onChange(function (newval) {
   circle.geometry = newGeometry; // 应用新的几何体到网格
   scene.add(circle); // 添加新的圆到场景
 });
-
 </script>
 
 <style scoped></style>
